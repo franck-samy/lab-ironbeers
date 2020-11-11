5abfdc4b19015d2096eb0e5bed0c676dc641c293
Franck SAMY <franck.samy@gmail.com>
2020-11-11 16:23:21 +0100

    iteration4

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Register the location for handlebars partials here:
 
@@ -18,8 +22,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers().then(beer => {
+    res.render("beers", { beers: beer });
+  });
+});
+
+app.get("/random-beers", (req, res) => {
+  punkAPI.getRandom().then(randomBeer => {
+    res.render("/random-beers", { random: randomBeer });
+  });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
